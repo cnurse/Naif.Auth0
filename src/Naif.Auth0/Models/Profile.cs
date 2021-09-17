49986d2f7bd4b5ae5816ce4f6da53033dea2faed
Charles Nurse <charlesnurse@gmail.com>
2021-09-17 13:38:03 -0700

    Add new Profile Model Up version to 2.1.0-beta.7

---
+++ src/Naif.Auth0/Models/Profile.cs
@@ -0,0 +1,11 @@
+namespace Naif.Auth0.Models
+{
+    public class Profile
+    {
+        public string Name { get; set; }
+        
+        public string EmailAddress { get; set; }
+        
+        public string ProfileImage { get; set; }
+    }
+}
